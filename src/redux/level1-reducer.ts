ab6a837a0dec55db1715ffa1f6b85000ecb683dd
Isckatel <nt-polza@yandex.ru>
2022-01-21 19:43:45 +0500

    position reducer

--- src/redux/level1-reducer.ts
+++ src/redux/level1-reducer.ts
@@ -1,7 +1,13 @@
 const SET_VISIBILITY_ITEM="SET_VISIBILITY_ITEM";
+const SET_DELTA_POSITION = "SET_DELTA_POSITION";
+
+type deltaPositionType = {x:number, y:number};
 
 export const setVisibilityItem = (id:number) => (
     {type:SET_VISIBILITY_ITEM, id}
+);
+export const setDeltaPosition = (id:number, deltaPosition:deltaPositionType) => (
+    {type:SET_DELTA_POSITION, id, deltaPosition}
 );
 
 type ActionTypes = ReturnType<typeof setVisibilityItem>;
@@ -22,14 +28,26 @@ let initialState = {
     ]
 }
 
-const level1Reducer = (state = initialState, action: ActionTypes) => {
+const level1Reducer = (state = initialState, action: any) => {
     switch (action.type) {
         case SET_VISIBILITY_ITEM:
             return {
                 ...state,
                 //переделать
                 items: state.items.map((item:any) => {
-                    if (item.id==action.id) return {...item, visibility: false}
+                    if (item.id==action.id) {return {...item, visibility: false}} 
+                    else {return {...item}}
+                })
+            }
+        case SET_DELTA_POSITION:
+            return {
+                ...state,
+                items: state.items.map((item:any) => {
+                    if (item.id==action.id) {
+                        return {...item, deltaPosition:{x:action.deltaPosition.x, y:action.deltaPosition.y}}
+                    } else {
+                        return {...item}
+                    }
                 })
             }
         default:
